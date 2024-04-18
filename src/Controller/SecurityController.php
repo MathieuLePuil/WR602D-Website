b80bdbb65982b34277481ff15d1ade707a610198
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-04-18 15:16:09 +0200

    feat: change locale to paris and secure generate pdf for login user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,23 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
@@ -30,9 +18,9 @@ class SecurityController extends AbstractController
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-//        if ($this->getUser()) {
-//            return $this->redirectToRoute('app_home');
-//        }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -77,9 +65,8 @@ class SecurityController extends AbstractController
 
                 $hashedPassword = $passwordHasher->hashPassword($user, $user->getPlainPassword());
                 $user->setPassword($hashedPassword);
-                $user->setCreatedAt(new \DateTimeImmutable());
-                $user->setUpdatedAt(new \DateTimeImmutable());
-
+                $user->setCreatedAt(new \DateTimeImmutable(null, new \DateTimeZone('Europe/Paris')));
+                $user->setUpdatedAt(new \DateTimeImmutable(null, new \DateTimeZone('Europe/Paris')));
                 $this->addFlash(
                     'success',
                     'Your account has been created. You can now log in.'
