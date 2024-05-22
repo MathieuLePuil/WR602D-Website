fdd10471a0db6acce62224e1600508720b773f6e
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-05-22 11:12:10 +0200

    feat: add cypress test for login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -71,7 +71,7 @@ class SecurityController extends AbstractController
                 $user->setUpdatedAt(new \DateTimeImmutable(null, new \DateTimeZone('Europe/Paris')));
                 $this->addFlash(
                     'success',
-                    'Your account has been created. You can now log in.'
+                    'Votre compte a été créé avec succès. Vous pouvez maintenant vous connecter.'
                 );
 
                 $manager->persist($user);
